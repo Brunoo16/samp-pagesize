f2a41bb7713d57af40b60bd31b081104e25445a8
BrunoBM16 <brunobmello2017@gmail.com>
2018-07-06 18:04:05 -0300

    No more c-style casts

--- samp-pagesize/dllmain.cpp
+++ samp-pagesize/dllmain.cpp
@@ -12,20 +12,20 @@ static void Thread()
 	offsets[1] = pattern.FindPattern("\x8B\x44\x24\x04\x83\xF8\x0A\x56\x8B\xF1\x7C\x16\x83\xF8\x64\x7F\x11\x89\x06\xE8\x00\x00\x00\x00\xC7\x86\x00\x00\x00\x00\x00\x00\x00\x00\x5E", "xxxxxxxxxxxxxxxxxxxx????xx????????x") + 0x6;
 	offsets[2] = pattern.FindPattern("\x5E\x59\xC3\xA1\x00\x00\x00\x00\x85\xC0\x74\x0E\x68\x00\x00\x00\x00\x50\xE8\x00\x00\x00\x00\x83\xC4\x08", "xxxx????xxxxx????xx????xxx") + 0xD;
 
-	VirtualProtect((void*)offsets[0], 1, PAGE_EXECUTE_READWRITE, &OldProtect);
-	VirtualProtect((void*)offsets[1], 1, PAGE_EXECUTE_READWRITE, &OldProtect);
-	VirtualProtect((void*)offsets[2], 4, PAGE_EXECUTE_READWRITE, &OldProtect);
+	VirtualProtect(reinterpret_cast<void*>(offsets[0]), 1, PAGE_EXECUTE_READWRITE, &OldProtect);
+	VirtualProtect(reinterpret_cast<void*>(offsets[1]), 1, PAGE_EXECUTE_READWRITE, &OldProtect);
+	VirtualProtect(reinterpret_cast<void*>(offsets[2]), 4, PAGE_EXECUTE_READWRITE, &OldProtect);
 
-	*(BYTE*)offsets[0] = 0x1;
-	*(BYTE*)offsets[1] = 0x1;
-	*(DWORD*)offsets[2] = (DWORD)&pagesize_string;
+	*reinterpret_cast<BYTE*>(offsets[0]) = 0x1;
+	*reinterpret_cast<BYTE*>(offsets[1]) = 0x1;
+	*reinterpret_cast<DWORD*>(offsets[2]) = reinterpret_cast<DWORD>(&pagesize_string);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
 	{
-		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Thread, 0, 0, 0);
+		CreateThread(0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(Thread), 0, 0, 0);
 	}
 	return TRUE;
 }
